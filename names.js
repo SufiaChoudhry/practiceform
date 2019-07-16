4a1494df0080ed96c053850bfae4c2120ef86ac3
SufiaChoudhry <49155770+SufiaChoudhry@users.noreply.github.com>
2019-07-16 17:55:20 +0500

    Add files via upload

---
+++ names.js
@@ -0,0 +1,4 @@
+let names = ["Nadir", "uzair", "sameeer", "waqas", "Talha", "shariq"];
+for (i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ script.js
@@ -0,0 +1,20 @@
+// function square(numb1){
+// return (numb1*n);
+//  }
+
+
+// function hello(name){
+// console.log("hello," + name);
+// }
+
+
+function check(){
+
+    let q1= document.myform.q1.value;
+    let count = 0;
+    if (q1=="a"){
+        count++;
+
+    }
+    alert("congratulations!!!! you got" + count + "marks");
+}
